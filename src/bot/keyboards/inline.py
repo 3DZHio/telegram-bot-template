d9a8913fb117dcab2a66996a5acb733399f576b2
3DZHio <79630008+3DZHio@users.noreply.github.com>
2024-01-08 09:29:49 +0200

    Telegram Bot Template

--- src/bot/keyboards/inline.py
+++ src/bot/keyboards/inline.py
@@ -1,5 +1,1 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-
-def inline_keyboard_markup(inline_keyboard: list[list[InlineKeyboardButton]]) -> InlineKeyboardMarkup:
-    return InlineKeyboardMarkup(inline_keyboard=inline_keyboard)
