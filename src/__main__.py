180abda77e67204adc72b28d569cf4e782c4e0a6
three <3dzhio@gmail.com>
2024-01-08 18:15:00 +0200

    Telegram Bot Template

--- src/__main__.py
+++ src/__main__.py
@@ -1,50 +1,52 @@
-from src.bot.loader import bot, dp
+from aiogram import Bot, Dispatcher
+from aiogram import F
+from aiogram.fsm.storage.memory import MemoryStorage
+
+from src.bot.handlers import get_routers
+from src.bot.utils import setup_logger, set_commands
+from src.config import settings
 from src.database.connection import pool
 
 
-def setup_filters() -> None:
+def setup_filters(dp: Dispatcher) -> None:
     """FILTERS"""
-    from aiogram import F
 
     dp.message.filter(F.chat.type == "private")  # Only Private Chat Filter
 
 
-def setup_handlers() -> None:
+def setup_handlers(dp: Dispatcher) -> None:
     """HANDLERS"""
-    from src.bot.handlers import get_routers
 
     dp.include_routers(*get_routers())  # Register Routers
 
 
-def setup_middlewares() -> None:
+def setup_middlewares(dp: Dispatcher) -> None:
     """MIDDLEWARES"""
-    from src.bot.middlewares.throttling import ThrottlingMiddleware
 
-    dp.message.middleware(ThrottlingMiddleware)
+    dp.message.middleware()  # Register MiddleWares
 
 
-@dp.startup()
-async def on_startup() -> None:
-    """STARTUP"""
-    from src.bot.utils import setup_logger, set_commands
+async def main() -> None:
+    bot = Bot(token=settings.BOT_TOKEN.get_secret_value(), parse_mode="HTML")  # Initializing Bot
+    storage = MemoryStorage()  # Create Storage
+    dp = Dispatcher(storage=storage)  # Launch Dispatcher
 
     setup_logger("INFO", ["aiogram.bot.api"])  # Logging
-    setup_filters()  # Filters
-    setup_handlers()  # Handlers
-    setup_middlewares()  # MiddleWares
+    setup_filters(dp)  # Filters
+    setup_handlers(dp)  # Handlers
+    setup_middlewares(dp)  # MiddleWares
     await set_commands(bot)  # Bot Commands
-    await pool.open()  # Open Connection Pool
 
-
-@dp.shutdown()
-async def on_shutdown() -> None:
-    """SHUTDOWN"""
-    await dp.storage.close()  # Close Storage
-    await pool.close()  # Close Connection Pool
-    await bot.session.close()  # Close Bot Session
+    try:
+        await pool.open()  # Open Connection Pool
+        await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())  # Start Polling
+    finally:
+        await pool.close()  # Close Connection Pool
+        await dp.storage.close()  # Close Storage
+        await bot.session.close()  # Close Bot Session
 
 
 if __name__ == '__main__':  # Entry Point
     from asyncio import run
 
-    run(dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types()))  # Start Polling
+    run(main())  # Launch Code
--- src/bot/loader.py
+++
@@ -1,8 +0,0 @@
-from aiogram import Bot, Dispatcher
-from aiogram.fsm.storage.memory import MemoryStorage
-
-from src.config import settings  # Prerequisites
-
-bot = Bot(token=settings.BOT_TOKEN.get_secret_value(), parse_mode="HTML")  # Initializing Bot
-storage = MemoryStorage()  # Create Storage
-dp = Dispatcher(storage=storage)  # Launch Dispatcher
