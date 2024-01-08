2be9214b8468b987bdb0de3fcf49b577ed1c784e
3DZHio <79630008+3DZHio@users.noreply.github.com>
2024-01-08 09:29:41 +0200

    Telegram Bot Template

--- src/bot/keyboards/reply.py
+++ src/bot/keyboards/reply.py
@@ -1,9 +1,1 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-
-def reply_keyboard_markup(keyboard: list[list[KeyboardButton]],
-                          resize_keyboard: bool = True,
-                          input_field_placeholder: str | None = None) -> ReplyKeyboardMarkup:
-    return ReplyKeyboardMarkup(keyboard=keyboard,
-                               resize_keyboard=resize_keyboard,
-                               input_field_placeholder=input_field_placeholder)
