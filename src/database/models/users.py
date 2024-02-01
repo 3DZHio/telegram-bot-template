b8ab72488e363c308885aa9254d5e4f2e1fff938
three <3dzhio@gmail.com>
2024-02-01 21:23:12 +0200

    Telegram Bot Template

--- src/database/models/users.py
+++ src/database/models/users.py
@@ -1,19 +1,19 @@
-from src.database.core.functions import transaction, fetchone
+from src.database.core import functions
 
 
 async def exists(uid: int) -> bool:
     """Check for Existence"""
-    return bool(await fetchone("SELECT 1 FROM users WHERE uid = %s LIMIT 1;",
-                               (uid,)))
+    return bool(await functions.fetchone("SELECT 1 FROM users WHERE uid = %s LIMIT 1;",
+                                         (uid,)))
 
 
 async def add(uid: int) -> None:
     """Add"""
-    await transaction("INSERT INTO users(uid) VALUES (%s);",
-                      (uid,))
+    await functions.transaction("INSERT INTO users(uid) VALUES (%s);",
+                                (uid,))
 
 
 async def info(uid: int) -> dict:
     """Information"""
-    return await fetchone("SELECT * FROM users WHERE uid = %s LIMIT 1;",
-                          (uid,))
+    return await functions.fetchone("SELECT * FROM users WHERE uid = %s LIMIT 1;",
+                                    (uid,))
